eb17e51b41509ebf9349b475f53ca63e7482c54b
Marcus Negron <mnegron40@stuy.edu>
2024-04-11 14:55:04 -0400

    database

--- grocery_list/grocery_list/groceries/models.py
+++ grocery_list/grocery_list/groceries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Grocery(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+class List(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    groceries = models.ManyToManyField(Grocery)
+    date_of_creation = models.DateTimeField()
+    
+    class Meta: ordering = ['name']
